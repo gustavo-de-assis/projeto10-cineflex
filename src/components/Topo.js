31e5d5c370a718f88e992b0a026bbb4c65ac08fc
Gustavo de Assis <gustavo.siqueira.421@gmail.com>
2022-10-04 22:12:42 -0300

    inicio do projeto. Parte superior

---
+++ src/components/Topo.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export default function Topo(){
+    return(
+        <ItemTopo>
+            CINEFLEX
+        </ItemTopo>
+    );
+}
+
+const ItemTopo = styled.div`
+    width: 375px;
+    height: 75px;
+    background-color: #C3CFD9;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    color: #E8833A;
+    font-size: 50px;
+
+`
